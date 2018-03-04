627e2b28f20dfe50a9b8eadcb3f655f8ba9502a8
Nanopulse1 <36006416+Nanopulse1@users.noreply.github.com>
2018-03-04 16:17:37 +0000

    Battle Scene V0.1
    Objects implemented, not coded as of yet

---
+++ ChannelChaser/Assets/Volcano/Scripts/BridgeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeController : MonoBehaviour {
+    //Variables
+    static public bool SwitchOn;
+    SpriteRenderer sprite;
+    Collider2D bridgeBlock;
+
+	// Use this for initialization
+	void Start () {
+        sprite = GetComponent<SpriteRenderer>();
+
+        sprite.enabled = true;
+        SwitchOn = false;
+
+        if (gameObject.tag == "Bridge")
+        {
+            sprite.enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(SwitchOn == true)
+        {
+            sprite.enabled = true;
+
+        }
+
+
+	}
+
+}
