c1e344fe442464673b1f0fbf556afbc1b7586291
S00181590 <36006911+S00181590@users.noreply.github.com>
2018-03-07 18:20:14 +0000

    Hopefully Finished World Map (No UI) or errors XD
    I have it all done, let's hope github doesn't fuck up

--- ChannelChaser/Assets/PlayableCharacter/Scripts/PlayerController.cs
+++ ChannelChaser/Assets/PlayableCharacter/Scripts/PlayerController.cs
@@ -7,11 +7,8 @@ public class PlayerController : MonoBehaviour {
 
     public float speed = 2;
     static public Rigidbody2D playerRB;
-<<<<<<< HEAD
      public static bool OnGrass;
 
-=======
->>>>>>> master
     // Use this for initialization
     void Start ()
     {
@@ -52,8 +49,6 @@ public class PlayerController : MonoBehaviour {
         
     }
 
-<<<<<<< HEAD
-=======
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Switch")
@@ -61,5 +56,4 @@ public class PlayerController : MonoBehaviour {
             BridgeController.SwitchOn = true;
         }
     }
->>>>>>> master
 }
